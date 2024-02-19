3f3816751f726a2a9ba59e445422313da1b6963e
hung-nv95 <hungnvse151468@fpt.edu.vn>
2024-02-19 17:31:30 +0700

    update qr order

--- src/pages/User/PaymentStatus/PaymentFail.js
+++ src/pages/User/PaymentStatus/PaymentFail.js
@@ -18,9 +18,9 @@ const CancelPayment = () => {
         <h2>Thanh toán thất bại</h2>
         <p>Đơn hàng của quý khách không thể hoàn tất thanh toán. </p>
         <Link to="/home">
-          <Button id="confirmBtn" variant="contained" color="primary">
+          <button className="bg-red-500 text-white p-2 rounded-3xl hover:bg-red-600 transition-colors mt-2">
             Quay về trang chủ
-          </Button>
+          </button>
         </Link>
       </div>
     </>
