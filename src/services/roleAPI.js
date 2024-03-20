8ca0d6f588b1552ef02e5bf9ada502d52067fa92
viethung <viethungdev23@gmail.com>
2024-03-20 05:12:19 +0000

    fix-roleApi

--- src/services/roleAPI.js
+++ src/services/roleAPI.js
@@ -3,9 +3,9 @@ import api from "./api";
 import axiosInstance from "./axiosConfig";
 
 const roleAPI = {
-  getRole: async (roleId) => {
+  getRole: async (unitId) => {
     try {
-      const response = await axios.get(`${api}/v1/roles/unit/${roleId}`);
+      const response = await axiosInstance.get(`${api}/v1/roles/unit/${unitId}`);
       return response.data;
     } catch (error) {
       throw error.response ? error.response.data : error.message;
